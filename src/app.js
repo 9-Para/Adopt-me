92c89e11578102c6356a54362fb8ddad25538203
rahul <asmrahul17@gmail.com>
2020-03-15 23:21:07 +0530

    added new files

--- src/app.js
+++ src/app.js
@@ -1,31 +1,26 @@
-const Pet = ({name,animal,breed}) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {},name ),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
-
+import React from "react";
+import { render } from "react";
+import Pet from "./Pet";
 const App = () => {
-  return React.createElement("div", { id: "Something important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
+   return React.createElement("div", { id: "Something important" }, [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+         name: "Luna",
+         animal: "Dog",
+         breed: "Havanese"
+      }),
 
-    React.createElement(Pet, {
-      name: "pepper",
-      animal: "Bird",
-      breed: "cocktail"
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "mixed"
-    })
-  ]);
+      React.createElement(Pet, {
+         name: "pepper",
+         animal: "Bird",
+         breed: "cocktail"
+      }),
+      React.createElement(Pet, {
+         name: "Doink",
+         animal: "Cat",
+         breed: "mixed"
+      })
+   ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
